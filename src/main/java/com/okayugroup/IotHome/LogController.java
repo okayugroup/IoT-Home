e05bd5fd5e0a0cf8d3b283d29e53fea782e164ff
yossy4411 <yaidonliving@gmail.com>
2024-05-31 21:46:39 +0900

    エラー問題を修正

--- src/main/java/com/okayugroup/IotHome/LogController.java
+++ src/main/java/com/okayugroup/IotHome/LogController.java
@@ -1,23 +1,26 @@
 package com.okayugroup.IotHome;
 
+import org.jetbrains.annotations.Nullable;
+
 public class LogController {
     public enum LogLevel {
         DEBUG,
         INFO,
-        WARNING,
         ERROR
     }
-    public static LogController LOGGER;
+    public static LogController LOGGER = new LogController(null);
+    @Nullable
     private final MainView view;
-    public LogController(MainView view){
+    public LogController(@Nullable MainView view){
         this.view = view;
         LOGGER = this;
     }
     public void log(String message) {
-        view.setLog(LogLevel.INFO, message);
+        log(LogLevel.INFO, message);
     }
     public void log(LogLevel level, String message) {
-        view.setLog(level, message);
+        if (view != null)
+            view.setLog(level, message);
     }
 
 }
