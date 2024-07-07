863772e3231492ccaa3bc384f679543d026e9294
yossy4411 <yaidonliving@gmail.com>
2024-07-07 14:51:10 +0900

    ソースファイルにライセンスを適用

--- src/main/java/com/okayugroup/IotHome/event/InputEvent.java
+++ src/main/java/com/okayugroup/IotHome/event/InputEvent.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Iot-Home.
+ *
+ * Iot-Home is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Iot-Home is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Iot-Home. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 OkayuGroup
+ */
+
 package com.okayugroup.IotHome.event;
 
 public abstract class InputEvent<T> extends Event<T> {
